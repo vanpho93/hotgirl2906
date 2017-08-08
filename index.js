2254168a2855905584b7f987cdede4037f637648
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-08 19:36:04 +0700

    eslint

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
 const app = express();
-app.listen(3000, () => console.log('Server started'));
-app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => res.render('home'))
+app.listen(3000, () => console.log('Server started'));
 
 // INSERT INTO public."HotGirl"(name, image, "like", dislike)
 // 	VALUES ('Mai Phuong Thuy', '1.jpg', 1, 3),
